14824a7ef555b2ac3898fe2861efbc7f18c07ce2
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2017-04-26 10:54:57 -0400

    Add project files.

---
+++ FHIR Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FHIR_Demo.MvcApplication" Language="C#" %>
---
+++ FHIR Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FHIR_Demo.Startup))]
+namespace FHIR_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
